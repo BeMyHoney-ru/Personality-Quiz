7c55b38e6316b8233f32e323312f51f361bd86b9
Михаил Емельянов <mishapt@icloud.com>
2018-09-06 22:04:19 +0300

    Setup UI

---
+++ Personality Quiz/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  Personality Quiz
+//
+//  Created by Михаил Емельянов on 06.09.2018.
+//  Copyright © 2018 Михаил Емельянов. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

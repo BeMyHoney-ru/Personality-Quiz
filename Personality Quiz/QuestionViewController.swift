ef7591ae8245cd568b72a2276711256c5e1f86f6
Михаил Емельянов <mishapt@icloud.com>
2018-09-10 20:55:23 +0300

    Questions ready

--- Personality Quiz/QuestionViewController.swift
+++ Personality Quiz/QuestionViewController.swift
@@ -10,21 +10,40 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    var questions: [Question] = [
+        Question(text: "Какая еда вам нравсится?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковь", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtle)
+                    
+            ]),
+        Question(text: "Что вам нравится делать?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Есть", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Прыгать", type: .rabbit),
+                    Answer(text: "Плавать", type: .turtle)
+            ]),
+        Question(text: "Насколько вам нравится поездки на машине?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Ненавижу их ", type: .cat),
+                    Answer(text: "Немного нервничаю", type: .rabbit),
+                    Answer(text: "Почти не замечаю", type: .turtle),
+                    Answer(text: "Обожаю их", type: .dog)
+            ])
+    ]
+    var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI() {
+        
     }
-    */
-
 }
